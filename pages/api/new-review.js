71df70037dc9b92cf545c875b9dbb98bbcdb3f4a
Csaba19999 <menesi777@gmail.com>
2022-08-24 12:44:40 +0200

    envirement variables added for the database connecting

--- pages/api/new-review.js
+++ pages/api/new-review.js
@@ -1,10 +1,11 @@
 import { MongoClient } from 'mongodb';
+import { env } from '../../next.config';
 
 async function handelr(req, res) {
   if(req.method === 'POST'){
     const data = req.body;
 
-    const client = await MongoClient.connect('mongodb+srv://Csaba1999:Xls50h..@thomasdcdatabase.64vbjp7.mongodb.net/?retryWrites=true&w=majority');
+    const client = await MongoClient.connect(`mongodb+srv://${process.env.DATABASE_CONNECT_NAME}:${process.env.DATABASE_CONNECT_PASSWORD}@thomasdcdatabase.64vbjp7.mongodb.net/?retryWrites=true&w=majority`);
     const db = client.db();
     const meetupsCollection = db.collection('reviews');
 
